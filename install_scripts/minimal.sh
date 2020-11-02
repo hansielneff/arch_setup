84d0130f3d6dd53f104e68f2d6c83c50284e1b28
Hans Otto Elneff <elneff@pm.me>
2020-11-02 22:36:24 +0100

    Completed minimal install script

---
+++ install_scripts/minimal.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+# This script sets up a minimal Arch Linux installation with GRUB.
+# Some parts will probably need to be modified in order to fit your needs.
+# 
+# Pre-requisites:
+# 	- UEFI boot mode
+# 	- A working internet connection.
+
+printf "Congratulations on making the right choice. (I use Arch btw)\n"
+
+printf "Enter desired hostname: "
+read hostname
+
+printf "$(lsblk)\n\nEnter name of the device you wish to install Arch onto: "
+read device
+
+# Update the system clock
+timedatectl set-ntp true
+timedatectl set-timezone Europe/Copenhagen
+
+# Partition, format and mount
+parted -s /dev/${device} mklabel gpt mkpart EFI fat32 1MiB 261MiB set 1 esp on mkpart Home ext4 261MiB 100%
+
+mkfs.fat -F32 /dev/${device}1
+mkfs.ext4 /dev/${device}2
+
+mkdir /mnt/boot
+mkdir /mnt/boot/efi
+mount /dev/${device}1 /mnt/boot/efi
+mount /dev/${device}2 /mnt
+
+# Install essential packages
+pacstrap /mnt base base-devel linux linux-firmware vim networkmanager git
+
+# Generate an fstab file
+genfstab -L /mnt >> /mnt/etc/fstab
+
+# Change root into the new system
+arch-chroot /mnt
+
+# Permanently set the system time
+ln -sf /usr/share/zoneinfo/Europe/Copenhagen /etc/localtime
+hwclock --systohc
+
+# Uncomment en_US.UTF-8 localization and generate
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+printf "LANG=en_US.UTF-8\n" > /etc/locale.conf
+printf "KEYMAP=dk\n" > /etc/vconsole.conf
+
+# Network configuration
+printf "${hostname}\n" > /etc/hostname
+printf "127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t${hostname}.localdomain\t${hostname}\n"
+systemctl enable --now NetworkManager
+
+# Set root password
+printf "Root password\n"
+passwd
+
+# Install Intel microcode
+pacman -S intel-ucode
+
+# Install and configure GRUB
+pacman -S grub efibootmgr
+grub-install --target=x86_64-efi --efi-directory=boot/efi --bootloader-id=GRUB
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
+
