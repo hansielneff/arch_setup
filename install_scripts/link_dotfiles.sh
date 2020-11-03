add2f0ba6a2d4a3a91470a945392381f8bec1f12
Hans Otto Elneff <elneff@pm.me>
2020-11-03 13:43:26 +0100

    Removed whitespace around =

--- install_scripts/link_dotfiles.sh
+++ install_scripts/link_dotfiles.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #This script creates symlinks to all the dotfiles
 
-dot_dir = "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../dotfiles"
+dot_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../dotfiles"
 
 # xinit
 ln -sf ${dot_dir}/xinitrc ~/.xinitrc
