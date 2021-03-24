d96b6f6e451a92b8e946915bb7b5ce0fab933543
Hans Otto Elneff <elneff@pm.me>
2021-03-24 02:47:08 +0100

    Added python, picom and GTK theme config

--- install_scripts/link_dotfiles.sh
+++ install_scripts/link_dotfiles.sh
@@ -3,10 +3,12 @@
 
 dot_dir="/home/$USER/arch_setup/dotfiles"
 
-ln -sf ${dot_dir}/xorg-xinit/xinitrc ~/.xinitrc
-ln -sf ${dot_dir}/xorg-xinit/xserverrc ~/.xserverrc
-ln -sf ${dot_dir}/st/config.h ~/packages/st/config.h
-ln -sf ${dot_dir}/st/PKGBUILD ~/packages/st/PKGBUILD
-ln -sf ${dot_dir}/dwm/config.h ~/packages/dwm/config.h
-ln -sf ${dot_dir}/dwm/PKGBUILD ~/packages/dwm/PKGBUILD
+ln -sf ${dot_dir}/xorg-xinit/xinitrc	~/.xinitrc
+ln -sf ${dot_dir}/xorg-xinit/xserverrc	~/.xserverrc
+ln -sf ${dot_dir}/picom/picom.conf	~/.config/picom.conf
+ln -sf ${dot_dir}/gtk3/settings.ini	~/.config/gtk-3.0/settings.ini
+ln -sf ${dot_dir}/st/config.h		~/packages/st/config.h
+ln -sf ${dot_dir}/st/PKGBUILD		~/packages/st/PKGBUILD
+ln -sf ${dot_dir}/dwm/config.h 		~/packages/dwm/config.h
+ln -sf ${dot_dir}/dwm/PKGBUILD 		~/packages/dwm/PKGBUILD
 
