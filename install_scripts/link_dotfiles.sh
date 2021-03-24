707960ba32722db963e7799e049426fde0c02962
Hans Otto Elneff <elneff@pm.me>
2021-03-24 05:31:40 +0100

    Added zsh

--- install_scripts/link_dotfiles.sh
+++ install_scripts/link_dotfiles.sh
@@ -7,6 +7,8 @@ ln -sf ${dot_dir}/xorg-xinit/xinitrc	~/.xinitrc
 ln -sf ${dot_dir}/xorg-xinit/xserverrc	~/.xserverrc
 ln -sf ${dot_dir}/picom/picom.conf	~/.config/picom.conf
 ln -sf ${dot_dir}/gtk3/settings.ini	~/.config/gtk-3.0/settings.ini
+ln -sf ${dot_dir}/zsh/zshrc		~/.config/zsh/.zshrc
+ln -sf ${dot_dir}/zsh/zprofile		~/.config/zsh/.zprofile
 ln -sf ${dot_dir}/st/config.h		~/packages/st/config.h
 ln -sf ${dot_dir}/st/PKGBUILD		~/packages/st/PKGBUILD
 ln -sf ${dot_dir}/dwm/config.h 		~/packages/dwm/config.h
