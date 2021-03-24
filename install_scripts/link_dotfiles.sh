9f5c971dc7a2afc7334f67f9130e23de26c8d882
Hans Otto Elneff <elneff@pm.me>
2021-03-24 02:10:30 +0100

    Fixed issue with dotfile linking

--- install_scripts/link_dotfiles.sh
+++ install_scripts/link_dotfiles.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 # This script creates symlinks to all the dotfiles
 
-dot_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/../dotfiles"
+dot_dir="/home/$USER/arch_setup/dotfiles"
 
-ln -sf "${dot_dir}"/xinitrc ~/.xinitrc
-ln -sf "${dot_dir}"/xserverrc ~/.xserverrc
+ln -sf ${dot_dir}/xorg-xinit/xinitrc ~/.xinitrc
+ln -sf ${dot_dir}/xorg-xinit/xserverrc ~/.xserverrc
+ln -sf ${dot_dir}/st/config.h ~/packages/st/config.h
+ln -sf ${dot_dir}/st/PKGBUILD ~/packages/st/PKGBUILD
+ln -sf ${dot_dir}/dwm/config.h ~/packages/dwm/config.h
+ln -sf ${dot_dir}/dwm/PKGBUILD ~/packages/dwm/PKGBUILD
+
