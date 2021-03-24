0cbaa5a8982fc9612730546bac5d969bb49c9da5
Hans Otto Elneff <elneff@pm.me>
2021-03-24 19:31:41 +0100

    create dirs before using them

--- install_scripts/link_dotfiles.sh
+++ install_scripts/link_dotfiles.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # This script creates symlinks to all the dotfiles
 
-dot_dir="/home/$USER/arch_setup/dotfiles"
+dot_dir="$HOME/arch_setup/dotfiles"
 
+mkdir $HOME/.config
 ln -sf ${dot_dir}/xorg-xinit/xinitrc	~/.xinitrc
 ln -sf ${dot_dir}/xorg-xinit/xserverrc	~/.xserverrc
 ln -sf ${dot_dir}/picom/picom.conf	~/.config/picom.conf
+mkdir $HOME/.config/gtk-3.0
 ln -sf ${dot_dir}/gtk3/settings.ini	~/.config/gtk-3.0/settings.ini
+mkdir $HOME/.config/zsh
 ln -sf ${dot_dir}/zsh/zshrc		~/.config/zsh/.zshrc
 ln -sf ${dot_dir}/zsh/zprofile		~/.config/zsh/.zprofile
 ln -sf ${dot_dir}/zsh/zshenv		~/.zshenv
@@ -14,4 +17,4 @@ ln -sf ${dot_dir}/st/config.h		~/packages/st/config.h
 ln -sf ${dot_dir}/st/PKGBUILD		~/packages/st/PKGBUILD
 ln -sf ${dot_dir}/dwm/config.h 		~/packages/dwm/config.h
 ln -sf ${dot_dir}/dwm/PKGBUILD 		~/packages/dwm/PKGBUILD
-
+ln -sf ${dot_dir}/thunderbird		~/.thunderbird
