fc4900cb88e2d36d7e400a5f5e605bf5b25b29af
Hans Otto Elneff <elneff@pm.me>
2021-04-21 20:32:25 +0200

    Added Starship prompt

--- install_scripts/link_dotfiles.sh
+++ install_scripts/link_dotfiles.sh
@@ -4,15 +4,18 @@
 dot_dir="$HOME/arch_setup/dotfiles"
 
 mkdir $HOME/.config
-ln -sf ${dot_dir}/xorg-xinit/xinitrc	~/.xinitrc
-ln -sf ${dot_dir}/xorg-xinit/xserverrc	~/.xserverrc
-ln -sf ${dot_dir}/i3/config		~/.config/i3/config
+ln -sf ${dot_dir}/xorg-xinit/xinitrc		~/.xinitrc
+ln -sf ${dot_dir}/xorg-xinit/xserverrc		~/.xserverrc
+ln -sf ${dot_dir}/i3/config			~/.config/i3/config
 mkdir $HOME/.config/i3status
-ln -sf ${dot_dir}/i3status/config	~/.config/i3status/config
-ln -sf ${dot_dir}/picom/picom.conf	~/.config/picom.conf
+ln -sf ${dot_dir}/i3status/config		~/.config/i3status/config
+ln -sf ${dot_dir}/picom/picom.conf		~/.config/picom.conf
 mkdir $HOME/.config/gtk-3.0
-ln -sf ${dot_dir}/gtk3/settings.ini	~/.config/gtk-3.0/settings.ini
+ln -sf ${dot_dir}/gtk3/settings.ini		~/.config/gtk-3.0/settings.ini
 mkdir $HOME/.config/zsh
-ln -sf ${dot_dir}/zsh/zshrc		~/.config/zsh/.zshrc
-ln -sf ${dot_dir}/zsh/zprofile		~/.config/zsh/.zprofile
-ln -sf ${dot_dir}/zsh/zshenv		~/.zshenv
+ln -sf ${dot_dir}/zsh/zshrc			~/.config/zsh/.zshrc
+ln -sf ${dot_dir}/zsh/zprofile			~/.config/zsh/.zprofile
+ln -sf ${dot_dir}/zsh/zshenv			~/.zshenv
+ln -sf ${dot_dir}/alacritty/alacritty.yml	~/.config/alacritty.yml
+ln -sf ${dot_dir}/starship/starship.toml	~/.config/starship.toml
+
