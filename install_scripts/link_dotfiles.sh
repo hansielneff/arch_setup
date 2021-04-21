90bc7dddb0d132d12d4fbe0876f118318d394e41
Hans Otto Elneff <elneff@pm.me>
2021-04-21 18:17:53 +0200

    Switched to i3

--- install_scripts/link_dotfiles.sh
+++ install_scripts/link_dotfiles.sh
@@ -6,6 +6,9 @@ dot_dir="$HOME/arch_setup/dotfiles"
 mkdir $HOME/.config
 ln -sf ${dot_dir}/xorg-xinit/xinitrc	~/.xinitrc
 ln -sf ${dot_dir}/xorg-xinit/xserverrc	~/.xserverrc
+ln -sf ${dot_dir}/i3/config		~/.config/i3/config
+mkdir $HOME/.config/i3status
+ln -sf ${dot_dir}/i3status/config	~/.config/i3status/config
 ln -sf ${dot_dir}/picom/picom.conf	~/.config/picom.conf
 mkdir $HOME/.config/gtk-3.0
 ln -sf ${dot_dir}/gtk3/settings.ini	~/.config/gtk-3.0/settings.ini
@@ -13,8 +16,3 @@ mkdir $HOME/.config/zsh
 ln -sf ${dot_dir}/zsh/zshrc		~/.config/zsh/.zshrc
 ln -sf ${dot_dir}/zsh/zprofile		~/.config/zsh/.zprofile
 ln -sf ${dot_dir}/zsh/zshenv		~/.zshenv
-ln -sf ${dot_dir}/st/config.h		~/packages/st/config.h
-ln -sf ${dot_dir}/st/PKGBUILD		~/packages/st/PKGBUILD
-ln -sf ${dot_dir}/dwm/config.h 		~/packages/dwm/config.h
-ln -sf ${dot_dir}/dwm/PKGBUILD 		~/packages/dwm/PKGBUILD
-ln -sf ${dot_dir}/thunderbird		~/.thunderbird
--- install_scripts/setup.sh
+++ install_scripts/setup.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 # This script configures a newly installed system to my preferences
+# This includes installing all the packages that I use
+
+# Set zsh to be the user's default shell
+chsh -s /bin/zsh
 
 # Link /bin/sh to Dash instead of slow, fat Bash
 sudo ln -sf /bin/dash /bin/sh
+
+# Enable the multilib repo for 32-bit packages
+sudo sed -i '/\[multilib\]/{s/#//;n;s/#//;}' /etc/pacman.conf
+sudo pacman -Sy
+
+# Install Paru to make installing AUR packages a breeze
+git clone https://aur.archlinux.org/paru
+cd paru
+makepkg -si
+cd ..
+rm -rf paru
+
+# Install the packages in package_list.txt
+paru -S $(cut -f1 -d' ' $HOME/arch_setup/package_list.txt)
+
